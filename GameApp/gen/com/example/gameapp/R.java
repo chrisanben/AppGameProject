f60cd65a381bb7435581d820b9a49f345a71f4a5
Chris <chrisanben@gmail.com>
2012-12-08 18:33:56 -0500

    Added new files

--- GameApp/gen/com/example/gameapp/R.java
+++ GameApp/gen/com/example/gameapp/R.java
@@ -32,10 +32,12 @@ public final class R {
     public static final class layout {
         public static final int activity_menu=0x7f030000;
         public static final int activity_options_menu=0x7f030001;
+        public static final int game_activity=0x7f030002;
     }
     public static final class menu {
         public static final int activity_menu=0x7f070000;
         public static final int activity_options_menu=0x7f070001;
+        public static final int game_activity=0x7f070002;
     }
     public static final class raw {
         public static final int menu_music=0x7f040000;
@@ -44,6 +46,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
+        public static final int title_activity_game=0x7f050004;
         public static final int title_activity_options_menu=0x7f050003;
     }
     public static final class style {
---
+++ GameApp/src/com/example/gameapp/GameActivity.java
@@ -0,0 +1,100 @@
+package com.example.gameapp;
+
+import java.util.Random;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class GameActivity extends Activity {
+	
+	private final long countBy = 1000;
+	
+	private long startTime = 5000;
+	private ImageButton[] button = new ImageButton[9];
+	private TextView timerTextView;
+	private TextView scoreTextView;
+	private LinearLayout gameLayout;
+	private MyCountDownTimer timer;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.game_activity);
+		
+		timerTextView = (TextView) findViewById(R.id.timerTextView);
+		scoreTextView = (TextView) findViewById(R.id.scoreTextView);
+		
+		timer = new MyCountDownTimer(startTime, countBy);
+		
+		gameLayout = (LinearLayout) findViewById(R.id.gameLayout);
+		//gameLayout.setOnClickListener(buttonClickListener);
+		gameLayout.setOnTouchListener(buttonTouchListener);
+		
+		button[0] = (ImageButton) findViewById(R.id.button0);
+		button[1] = (ImageButton) findViewById(R.id.button1);
+		button[2] = (ImageButton) findViewById(R.id.button2);
+		button[3] = (ImageButton) findViewById(R.id.button3);
+		button[4] = (ImageButton) findViewById(R.id.button4);
+		button[5] = (ImageButton) findViewById(R.id.button5);
+		button[6] = (ImageButton) findViewById(R.id.button6);
+		button[7] = (ImageButton) findViewById(R.id.button7);
+		button[8] = (ImageButton) findViewById(R.id.button8);
+		/*for (int i = 0; i < 9; i++) {
+			button[i].setOnClickListener(buttonClickListener);
+		}*/
+		
+		for (int i = 0; i < 9; i++) {
+			button[i].setOnTouchListener(buttonTouchListener);
+		}
+		
+		timer.start();
+			
+	}
+
+	public class MyCountDownTimer extends CountDownTimer {
+		
+		public MyCountDownTimer(long startTime, long countBy) {
+			super(startTime, countBy);
+		}
+		
+		//when the timer finishes and they haven't cleared all the cards
+		//tell them they lost and disable the buttons because the Alert Dialog doesn't work
+		@Override
+		public void onFinish() {
+			timerTextView.setText("FINISHED");
+			/*if (allCleared == false) {
+				winTextView.setText(R.string.you_lose);
+				// AlertDialog.Builder(endGame);
+				for (int row = 0; row < numOfRows; row++) {
+					for (int col = 0; col < rowObjects; col++) {
+						cardImage[row][col].setEnabled(false);
+					}
+				}
+			}*/
+		}
+		
+		//tick the timer down by 1 second
+		@Override
+		public void onTick(long millisUntilFinished) {
+			timerTextView.setText(String.valueOf(millisUntilFinished));
+		}
+	}
+	public int randomButton() {
+		int rButton;
+		Random random = new Random();
+		rButton = random.nextInt(9);
+		return rButton;
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.game_activity, menu);
+		return true;
+	}
+
+}
